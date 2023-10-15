fd90b094276933f77e95cd43e9958cc8f8f2ceb4
Vladislav <lezver1994@gmail.com>
2023-10-15 22:31:45 +0300

    add logic in modal window

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -2,16 +2,21 @@ import ReactDOM from 'react-dom';
 import './Overlay.scss';
 import { Articale } from '../';
 import { RxCross2 } from 'react-icons/rx';
+import { useEffect } from 'react';
 
 export const Overlay = ({ isOpen, car }) => {
+  // eslint-disable-next-line
+  useEffect(() => (document.body.style.overflow = 'hidden'), []);
+
+  const closeModal = () => {
+    isOpen(false);
+    document.body.style.overflow = 'visible';
+  };
+
   return ReactDOM.createPortal(
     <div className="overlay">
       <div>
-        <button
-          className="overlay__cross"
-          type="button"
-          onClick={() => isOpen(false)}
-        >
+        <button className="overlay__cross" type="button" onClick={closeModal}>
           <RxCross2 size={20} />
         </button>
         <Articale car={car} />
